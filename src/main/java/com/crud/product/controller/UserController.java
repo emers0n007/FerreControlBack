495f77f12395a95b5f409f8420d2d6e1d15decb4
emers0n007 <emersondb_07@hotmail.com>
2024-05-06 22:24:39 -0500

    Security

--- src/main/java/com/crud/product/controller/UserController.java
+++ src/main/java/com/crud/product/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.crud.product.controller;
 
 import com.crud.product.model.LoginRequest;
+import com.crud.product.model.ServiceResponse;
 import com.crud.product.model.Users;
 import com.crud.product.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,17 +20,39 @@ public class UserController {
     @Autowired
     private IUserService iUserService;
 
+    private PasswordEncoder passwordEncoder ;
+
     @PostMapping("/login")
     public ResponseEntity<Users> findAll(@RequestBody LoginRequest loginRequest){
+        passwordEncoder =  new BCryptPasswordEncoder();
         var listUsers = iUserService.findAll();
         Users user = new Users();
         for (Users users:listUsers) {
             if(users.getName_user().compareToIgnoreCase(loginRequest.getName_user())==0){
-                if(users.getPassword().compareToIgnoreCase(loginRequest.getPassword())==0){
+                if (passwordEncoder.matches(loginRequest.getPassword(), users.getPassword())) {
                     user = users;
+                    break;
                 }
             }
         }
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // Contraseña incorrecta
+        }
+    }
+
+    @PostMapping("/save/user")
+    public ResponseEntity<ServiceResponse> saveUser(@RequestBody Users users){
+        ServiceResponse serviceResponse =  new ServiceResponse();
+        int result = iUserService.save(users);
+        if(result==1){
+            serviceResponse.setMessage("Agregado Con Exito");
+            serviceResponse.setSuccess(true);
+        }else{
+            serviceResponse.setMessage("No se agrego Correctamente");
+            serviceResponse.setSuccess(false);
+        }
+        return new ResponseEntity<>(serviceResponse, HttpStatus.OK);
     }
 }
--- src/main/java/com/crud/product/repository/UserRepository.java
+++ src/main/java/com/crud/product/repository/UserRepository.java
@@ -10,12 +10,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository implements IUserRepository{
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    private PasswordEncoder passwordEncoder;
     @Override
     public List<Users> findAll() {
         String SQL = "SELECT * FROM users";
@@ -23,8 +27,11 @@ public class UserRepository implements IUserRepository{
     }
 
     @Override
-    public int save(Users users) {
-        return 0;
+    public int save(Users user) {
+        passwordEncoder =  new BCryptPasswordEncoder();
+        String SQL = "INSERT INTO Users (name_user, password, name, role) VALUES (?, ?, ?, ?)";
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        return jdbcTemplate.update(SQL, user.getName_user(), hashedPassword, user.getName(), user.getRole());
     }
 
     @Override
